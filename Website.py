12f289f1f529b9449be2ee5abc3a1508ccb0bcf1
jordandos <118776009+jordandos@users.noreply.github.com>
2023-11-26 11:09:48 -0500

    Update Website.py
    Expanding on the code

--- Website.py
+++ Website.py
@@ -12,18 +12,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'your_database_uri'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Define database models (replace 'User' and 'Course' with your actual models)
+# Define database models (replace 'User', 'Course', and 'Advisor' with your actual models)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    # Add other fields based on your user profile
+    courses = db.relationship('Course', backref='user', lazy=True)  # Relationship with Course model
 
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     course_name = db.Column(db.String(120), nullable=False)
     prerequisites = db.Column(db.String(255))
-    # Add other fields based on your course model
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Foreign key relationship
+
+class Advisor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
 # Define routes and endpoints
 @app.route('/login', methods=['POST'])
@@ -31,11 +36,16 @@ def login():
     try:
         data = request.get_json()
 
-        # Validate user credentials (replace 'validate_credentials' with your validation logic)
-        if validate_credentials(data['username'], data['password']):
-            return jsonify({'message': 'Login successful'}), 200
+        # Validate user credentials
+        user = User.query.filter_by(username=data['username'], password=data['password']).first()
+        advisor = Advisor.query.filter_by(username=data['username'], password=data['password']).first()
+
+        if user:
+            return jsonify({'message': 'Login successful', 'user_type': 'student'}), 200
+        elif advisor:
+            return jsonify({'message': 'Login successful', 'user_type': 'advisor'}), 200
         else:
-            return jsonify({'message': 'Invalid credentials'}), 401
+            return jsonify({'error': 'Invalid credentials'}), 401
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
@@ -44,11 +54,25 @@ def register():
     try:
         data = request.get_json()
 
-        # Create a new user in the database (replace 'create_user' with your user creation logic)
-        create_user(data['username'], data['password'])
+        # Check if the username is already taken
+        if User.query.filter_by(username=data['username']).first() or Advisor.query.filter_by(username=data['username']).first():
+            return jsonify({'error': 'Username is already taken'}), 400
+
+        # Create a new user or advisor in the database based on user_type
+        if data['user_type'] == 'student':
+            new_user = User(username=data['username'], password=data['password'])
+            db.session.add(new_user)
+        elif data['user_type'] == 'advisor':
+            new_advisor = Advisor(username=data['username'], password=data['password'])
+            db.session.add(new_advisor)
+
+        db.session.commit()
         return jsonify({'message': 'Registration successful'}), 201
     except Exception as e:
+        db.session.rollback()
         return jsonify({'error': str(e)}), 500
+    finally:
+        db.session.close()
 
 @app.route('/courses', methods=['GET'])
 def get_courses():
@@ -60,20 +84,49 @@ def get_courses():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-@app.route('/update_user', methods=['PUT'])
-def update_user():
+@app.route('/user_profile', methods=['GET'])
+def get_user_profile():
     try:
         data = request.get_json()
 
-        # Update user information in the database (replace 'update_user_info' with your logic)
-        update_user_info(data['username'], data['new_password'])
-        return jsonify({'message': 'User information updated successfully'}), 200
+        # Fetch user profile information based on username
+        user = User.query.filter_by(username=data['username']).first()
+        if user:
+            user_profile = {
+                'username': user.username,
+                'courses': [{'course_name': course.course_name, 'prerequisites': course.prerequisites} for course in user.courses]
+            }
+            return jsonify({'user_profile': user_profile}), 200
+        else:
+            return jsonify({'error': 'User not found'}), 404
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-# Add other routes for different functionalities based on your requirements
+@app.route('/advisor_profile', methods=['GET'])
+def get_advisor_profile():
+    try:
+        data = request.get_json()
 
-# Run the application
-if __name__ == '__main__':
-    db.create_all()  # Create database tables before running the app
-    app.run(debug=True)
+        # Fetch advisor profile information based on username
+        advisor = Advisor.query.filter_by(username=data['username']).first()
+        if advisor:
+            advisor_profile = {
+                'username': advisor.username,
+                'students': [user.username for user in User.query.all()]  # Get all students for simplicity
+            }
+            return jsonify({'advisor_profile': advisor_profile}), 200
+        else:
+            return jsonify({'error': 'Advisor not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/upload_resource', methods=['POST'])
+def upload_resource():
+    try:
+        data = request.get_json()
+
+        # Save uploaded resource to the database or file system
+        # (Implementation details depend on where and how you want to store the resources)
+        # For simplicity, we'll just return a success message here
+        return jsonify({'message': 'Resource uploaded successfully'}), 200
+    except Exception as
